d077d4cb7fe74764a6b0299c5447728cb0d4df9a
Kenneth Gottschalk de Azevedo <kenneth.azevedo@meutudo.app>
2025-03-04 17:04:07 -0300

    fist commit

--- src/main/java/com/example/jvms/controller/TestController.java
+++ src/main/java/com/example/jvms/controller/TestController.java
@@ -18,12 +18,22 @@ public class TestController {
   @GetMapping("/info")
   public Map<String, String> getJvmInfo() {
     Map<String, String> info = new HashMap<>();
+
     info.put("java.version", System.getProperty("java.version"));
     info.put("java.vendor", System.getProperty("java.vendor"));
     info.put("java.vm.name", System.getProperty("java.vm.name"));
     info.put("java.vm.version", System.getProperty("java.vm.version"));
     info.put("java.vm.vendor", System.getProperty("java.vm.vendor"));
     info.put("java.compiler", System.getProperty("java.compiler") == null ? "JIT" : System.getProperty("java.compiler"));
+
+    info.put("java.class.version", System.getProperty("java.class.version"));
+    info.put("java.specification.version", System.getProperty("java.specification.version"));
+    info.put("java.vm.specification.version", System.getProperty("java.vm.specification.version"));
+
+    info.put("os.name", System.getProperty("os.name"));
+    info.put("os.arch", System.getProperty("os.arch"));
+    info.put("os.version", System.getProperty("os.version"));
+
     return info;
   }
 
